3b79ad3d196c3ba7a628a67c4ecff7fcf6e812e3
Ariel Mirra <ariel.mirra@ing.austral.edu.ar>
2020-10-08 18:25:17 -0300

    add get_top_five_naive_bayes_words

--- src/spam/spam.py
+++ src/spam/spam.py
@@ -195,7 +195,18 @@ def get_top_five_naive_bayes_words(model, dictionary):
 
     Returns: A list of the top five most indicative words in sorted order with the most indicative first
     """
-    return
+    spams = model[1]
+    dictionary_keys = list(dictionary.keys())
+    words = []
+    for i, triple in enumerate(spams):
+        word = dictionary_keys[i]
+        word_mean = spams[i][0]
+        words.append((word, word_mean))
+
+    words.sort(key=(lambda w: w[1]))
+    top5 = list(map(lambda t: t[0], words[-5:]))
+    top5.reverse()
+    return top5
 
 
 def compute_best_svm_radius(train_matrix, train_labels, val_matrix, val_labels, radius_to_consider):
@@ -219,49 +230,49 @@ def compute_best_svm_radius(train_matrix, train_labels, val_matrix, val_labels, 
 
 def main():
     train_messages, train_labels = util.load_spam_dataset('spam_train.tsv')
-    val_messages, val_labels = util.load_spam_dataset('spam_val.tsv')
-    test_messages, test_labels = util.load_spam_dataset('spam_test.tsv')
+    # val_messages, val_labels = util.load_spam_dataset('spam_val.tsv')
+    # test_messages, test_labels = util.load_spam_dataset('spam_test.tsv')
 
     dictionary = create_dictionary(train_messages)
 
     print('Size of dictionary: ', len(dictionary))
 
-    util.write_json('spam_dictionary', dictionary)
+    # util.write_json('spam_dictionary', dictionary)
 
     train_matrix = transform_text(train_messages, dictionary)
 
-    np.savetxt('spam_sample_train_matrix', train_matrix[:100, :])
+    # np.savetxt('spam_sample_train_matrix', train_matrix[:100, :])
 
-    val_matrix = transform_text(val_messages, dictionary)
-    test_matrix = transform_text(test_messages, dictionary)
+    # val_matrix = transform_text(val_messages, dictionary)
+    # test_matrix = transform_text(test_messages, dictionary)
 
     naive_bayes_model = fit_naive_bayes_model(train_matrix, train_labels)
 
-    naive_bayes_predictions = predict_from_naive_bayes_model(naive_bayes_model, test_matrix)
-
-    np.savetxt('spam_naive_bayes_predictions', naive_bayes_predictions)
-
-    naive_bayes_accuracy = np.mean(naive_bayes_predictions == test_labels)
-
-    print('Naive Bayes had an accuracy of {} on the testing set'.format(naive_bayes_accuracy))
+    # naive_bayes_predictions = predict_from_naive_bayes_model(naive_bayes_model, test_matrix)
+    #
+    # np.savetxt('spam_naive_bayes_predictions', naive_bayes_predictions)
+    #
+    # naive_bayes_accuracy = np.mean(naive_bayes_predictions == test_labels)
+    #
+    # print('Naive Bayes had an accuracy of {} on the testing set'.format(naive_bayes_accuracy))
 
     top_5_words = get_top_five_naive_bayes_words(naive_bayes_model, dictionary)
 
     print('The top 5 indicative words for Naive Bayes are: ', top_5_words)
 
-    util.write_json('spam_top_indicative_words', top_5_words)
-
-    optimal_radius = compute_best_svm_radius(train_matrix, train_labels, val_matrix, val_labels, [0.01, 0.1, 1, 10])
-
-    util.write_json('spam_optimal_radius', optimal_radius)
-
-    print('The optimal SVM radius was {}'.format(optimal_radius))
-
-    svm_predictions = svm.train_and_predict_svm(train_matrix, train_labels, test_matrix, optimal_radius)
-
-    svm_accuracy = np.mean(svm_predictions == test_labels)
-
-    print('The SVM model had an accuracy of {} on the testing set'.format(svm_accuracy, optimal_radius))
+    # util.write_json('spam_top_indicative_words', top_5_words)
+    #
+    # optimal_radius = compute_best_svm_radius(train_matrix, train_labels, val_matrix, val_labels, [0.01, 0.1, 1, 10])
+    #
+    # util.write_json('spam_optimal_radius', optimal_radius)
+    #
+    # print('The optimal SVM radius was {}'.format(optimal_radius))
+    #
+    # svm_predictions = svm.train_and_predict_svm(train_matrix, train_labels, test_matrix, optimal_radius)
+    #
+    # svm_accuracy = np.mean(svm_predictions == test_labels)
+    #
+    # print('The SVM model had an accuracy of {} on the testing set'.format(svm_accuracy, optimal_radius))
 
 
 if __name__ == "__main__":
